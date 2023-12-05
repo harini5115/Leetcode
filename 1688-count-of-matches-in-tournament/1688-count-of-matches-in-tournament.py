4b9bdd771ac46e1afa25037d9e293b394e17b274
Harini <harini5115@gmail.com>
2023-12-05 06:50:21 -0800

    Time: 15 ms (46.49%), Space: 13.2 MB (66.10%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # ans = 0
+        # while (n>1):
+        #     ans += n//2
+        #     new_n = n//2
+        #     new_r = n%2
+        #     n = new_n+new_r
+        return n-1
