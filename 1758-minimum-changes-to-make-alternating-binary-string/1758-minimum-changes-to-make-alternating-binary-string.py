bf74d809373b59aaa4b46cc2ed9f5468f96568ef
Harini <harini5115@gmail.com>
2023-12-24 11:32:45 -0800

    Time: 22 ms (89.55%), Space: 13.5 MB (72.06%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -4,31 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        prev1 = s[0]
-        if prev1=='0':
-            prev2 = '1'
-        else:
-            prev2 = '0'
-        n1 = 0
-        n2 = 1
-        for i in range(1, len(s)):
-            cur = s[i]
-            cur1 = cur
-            cur2 = cur
-            if prev1==cur1:
-                n1 +=1
-                if cur1=='0':
-                    cur1  = '1'
+        start0 = 0
+        start1 = 0
+        
+        for i in range(len(s)):
+            if i % 2 == 0:
+                if s[i] == "0":
+                    start1 += 1
                 else:
-                    cur1 = '0'
-            if prev2== cur2:
-                n2 +=1
-                if cur2=='0':
-                    cur2 = '1'
+                    start0 += 1
+            else:
+                if s[i] == "1":
+                    start1 += 1
                 else:
-                    cur2 = '0'
-            prev1 = cur1
-            prev2 = cur2
-        return min(n1, n2)
+                    start0 += 1
+        
+        return min(start0, start1)
+        
                     
                 
