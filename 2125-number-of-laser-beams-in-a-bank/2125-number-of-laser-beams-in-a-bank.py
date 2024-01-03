bbac76c01980b402138ee09cfb8398413c802a65
Harini <harini5115@gmail.com>
2024-01-02 22:40:20 -0800

    Time: 437 ms (10.23%), Space: 16.6 MB (97.73%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -4,23 +4,17 @@ class Solution(object):
         :type bank: List[str]
         :rtype: int
         """
-        row_count = [0]*len(bank)
-        for i in range(len(bank)):
-            row_count[i] = sum([1 if x=='1' else 0 for x in bank[i]])
-        ans = 0   
+        ans = 0
         prev = 0
-        while(prev<len(row_count) and row_count[prev]==0):
-            prev +=1
-        
-        nex = prev+1
-        while(nex<len(row_count)):
-            while(nex<len(row_count) and row_count[nex]==0):
-                nex +=1
-        
-            if nex<len(row_count):
-                ans +=row_count[prev]*row_count[nex]
-                prev = nex
-                nex = nex+1
+        for s in bank:
+            count = 0
+            for c in s:
+                if c=='1':
+                    count+=1
+            if count!=0:
+                ans+=prev*count
+                prev = count
+                    
         return ans
         
             
